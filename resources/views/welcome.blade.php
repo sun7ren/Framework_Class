fb7c37f41d5177c293125811f39565dd8506a11d
sun7ren <155711234+sun7ren@users.noreply.github.com>
2025-09-03 11:48:58 +0700

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome to the First Lesson</h1>
+    <p>Click the button below to view the list of students</p>
+
+    <a href="" class="btn">
+        View Students
+    </a>
+</body>
+</html>
