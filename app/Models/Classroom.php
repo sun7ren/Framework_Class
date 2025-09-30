2cf3e52f93d6a23fb40c9993497af177ab6ec7ef
sun7ren <155711234+sun7ren@users.noreply.github.com>
2025-09-30 22:13:27 +0700

    Task 16: Foreign keys PT.1

---
+++ app/Models/Classroom.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Classroom extends Model
+{
+    protected $fillable = ['name', 'description', 'location'];
+    /** @use HasFactory<\Database\Factories\ClassroomFactory> */
+    use HasFactory;
+}
